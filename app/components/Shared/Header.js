0db934a4dd92bba12f7c4f031a709a37d40f14a0
ahmmedrasel-dev <rahmmed.info@gmail.com>
2024-03-09 16:04:05 +0600

    update search blog

--- app/components/Shared/Header.js
+++ app/components/Shared/Header.js
@@ -1,12 +1,14 @@
 
+import { getAllArticles } from "@/app/utils/getAllArticles";
 import Navbar from "./Navbar";
 import { getCategories } from "@/app/utils/getCategories";
 
 export default async function Header() {
   const {data: categories} = await getCategories();
+  const {data: articles} = await getAllArticles();
   return (
     <>
-      <Navbar categories={categories} />
+      <Navbar categories={categories} articles={articles}/>
     </>
   );
 }
--- app/components/Shared/Navbar.js
+++ app/components/Shared/Navbar.js
@@ -1,17 +1,42 @@
 "use client";
 import { abril } from "@/app/fonts";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { FaSearch } from "react-icons/fa";
 import MobileNav from "./MobileNav";
 import DesktopNav from "./DesktopNav";
 import { FaAlignJustify } from "react-icons/fa";
 import Link from "next/link";
 import Headroom from "react-headroom";
+import Articles from "../LatestArticles/Articles";
 
-export default function Navbar({categories}) {
+export default function Navbar({categories, articles}) {
   const [isOpen, setIsOpen] = useState(false);
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
+  const [keyword, setKeyword] = useState("");
+  const [searchResult, setSetSearchResult] = useState([]);
+  const inputRef = useRef(null);
   const toggleMenu = () => {
     setIsOpen(!isOpen);
+  };
+
+  const handleSearchChange = (event) => {
+    
+    setKeyword(event.target.value);
+    const reuslt = articles.filter((article) =>
+      article.title.toLowerCase().includes(keyword.toLowerCase())
+    );
+    setSetSearchResult([...reuslt])
+  };
+  const handleFocus = () => {
+    setIsSearchOpen(true);
+  };
+
+  const handleBlur = () => {
+    
+    setTimeout(()=>{
+      setIsSearchOpen(false);
+      setSetSearchResult([])
+    }, 100)
   };
 
   return (
@@ -20,7 +45,7 @@ export default function Navbar({categories}) {
     tolerance={{ up: 10, down: 20 }}
     className={`transition-all duration-300`}
     style={{ zIndex: 2 }}>
-      <div className="xl:container mx-auto p-4 bg-white">
+      <div className="xl:container mx-auto p-4 bg-white relative">
           <nav className={`flex items-center justify-between`}>
               <Link href={'/'}>
                 <div className={`text-4xl text-heading_color ${abril.className}`}>
@@ -48,14 +73,29 @@ export default function Navbar({categories}) {
                 isOpen ? "hidden" : "block"
               }`}>
             <input
-              type="search"
+              type="text"
               placeholder="Search Here..."
               className="w-full border border-[#757575] px-6 py-3 rounded-full focus:outline-none focus:border-blue-500"
+              onChange={handleSearchChange}
+              ref={inputRef}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
             />
             <div className="absolute right-6 top-3">
               <FaSearch size={30} />
             </div>
           </div>
+
+          {isSearchOpen &&  (
+            <div className="search-results-container p-4 rounded-md">
+              {keyword.length > 0 && <h2 className="text-2xl py-3">Showing {searchResult.length} results for "{keyword}"</h2>}
+              <div className="grid grid-cols-4 gap-4">
+              {
+                searchResult.map( article => <Articles article={article} />)
+              }
+              </div>
+            </div>
+          )}
         </div>
     </Headroom>
   );
